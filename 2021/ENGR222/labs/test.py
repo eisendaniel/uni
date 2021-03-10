b12302ae75a0d73f61abaa87adac7337ba24b480
eisendaniel <danieleisen99@gmail.com>
2021-03-11 09:00:10 +1300

    222 python fix

--- 2021/ENGR222/labs/test.py
+++ 2021/ENGR222/labs/test.py
@@ -2,13 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
-t = np.linspace(0,2*np.pi,10000)
-x = np.sin(1*t)
-y = np.sin(0.5*t)
-z = np.cos(3*t)
-ax.plot(x,y,z)
-plt.show()
+def para3d():
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    t = np.linspace(0,2*np.pi,10000)
+    x = np.sin(1*t)
+    y = np.sin(0.5*t)
+    z = np.cos(3*t)
+    ax.plot(x,y,z)
+    plt.show()
+
+def surf():
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    # Make data.
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                        linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(-1.01, 1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    # A StrMethodFormatter is used automatically
+    ax.zaxis.set_major_formatter('{x:.02f}')
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
+
+surf()
