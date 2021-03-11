3430e89f90f4d710a1e0e21895446dfa8254af7a
eisendaniel <danieleisen99@gmail.com>
2021-03-12 10:21:20 +1300

    wk3 compl

--- 2021/ENGR222/labs/test.py
+++ 2021/ENGR222/labs/test.py
@@ -24,15 +24,14 @@ def surf():
     X = np.arange(-5, 5, 0.25)
     Y = np.arange(-5, 5, 0.25)
     X, Y = np.meshgrid(X, Y)
-    R = np.sqrt(X**2 + Y**2)
-    Z = np.sin(R)
+    Z = X**2 + Y**2
 
     # Plot the surface.
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                         linewidth=0, antialiased=False)
 
     # Customize the z axis.
-    ax.set_zlim(-1.01, 1.01)
+    # ax.set_zlim(-1.01, 1.01)
     ax.zaxis.set_major_locator(LinearLocator(10))
     # A StrMethodFormatter is used automatically
     ax.zaxis.set_major_formatter('{x:.02f}')
