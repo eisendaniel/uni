7d2bb985ad817de9475725d585feb64eb4c97d0d
eisendaniel <danieleisen99@gmail.com>
2021-03-15 23:42:33 +1300

    1.c

--- 2021/ENGR222/ass/ass_2/engr222_ass2_eisendani.py
+++ 2021/ENGR222/ass/ass_2/engr222_ass2_eisendani.py
@@ -17,17 +17,12 @@ def para3d():
     ax.plot(x,y,z)
     plt.show()
 
-def surf():
+def surf(x,y,z,title):
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-    # Make data.
-    x = np.arange(-8, 8, 0.1)
-    y = np.arange(-8, 8, 0.1)
-    x, y = np.meshgrid(x, y)
-    z = -2*x**3 + 3*(x**2)*y + 2*y**3 - 9*y + 5
-
+    plt.title(f"{title}")
+   
     # Plot the surface.
-    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+    surf = ax.plot_surface(x, y, z, cmap=cm.plasma,linewidth=0, antialiased=False)
 
     # Customize the z axis.
     # ax.set_zlim(-1.01, 1.01)
@@ -38,6 +33,19 @@ def surf():
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
 
-    plt.show()
+    
 
-surf()
+
+
+# Make data.
+x = np.arange(-10, 10, 1)
+y = np.arange(-10, 10, 1)
+x, y = np.meshgrid(x, y)
+z = -2*x**3 + 3*(x**2)*y + 2*y**3 - 9*y + 5
+fx = -6*x**2 + 6*x*y
+fy = 3*x**2 + 6*y**2 - 9
+
+surf(x,y,z,"F")
+surf(x,y,fx,"F_x")
+surf(x,y,fy,"F_y")
+plt.show()
