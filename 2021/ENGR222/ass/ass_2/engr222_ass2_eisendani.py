6c528192ddbfc41c7f8cf7bd00afc2008881c98d
eisendaniel <danieleisen99@gmail.com>
2021-03-23 14:23:24 +1300

    compl

--- 2021/ENGR222/ass/ass_2/engr222_ass2_eisendani.py
+++ 2021/ENGR222/ass/ass_2/engr222_ass2_eisendani.py
@@ -1,45 +1,104 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib as mpl
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
+from scipy.integrate import quad
 
 
-def para3d():
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+def q4ai():
+    f = lambda x:np.exp(np.cos(np.pi*x**2))
+    x_0 = 1/(np.sqrt(2))
+    h = 0.1**np.linspace(0, 21, 100)          
+    dfdx = (f(x_0+h)-f(x_0))/h #1st estimation
+    
+    error = np.abs(dfdx-(-np.sqrt(2)*np.pi))
 
-    t = np.linspace(0,2*np.pi,10000)
-    x = np.sin(1*t)
-    y = np.sin(0.5*t)
-    z = np.cos(3*t)
-    ax.plot(x,y,z)
+    print(f'Min e of {np.min(error)} at h = {h[np.argmin(error)]}')
+    plt.loglog(h,error)
+    plt.gca().invert_xaxis()
+    plt.xlabel('h')
+    plt.ylabel('error')
+    plt.grid()
+    plt.show()
 
-def surf(x,y,z,title):
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    plt.title(f"{title}")
-   
-    # Plot the surface.
-    surf = ax.plot_surface(x, y, z, cmap=cm.plasma,linewidth=0, antialiased=False)
 
-    # Customize the z axis.
-    # ax.set_zlim(-1.01, 1.01)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    # A StrMethodFormatter is used automatically
-    ax.zaxis.set_major_formatter('{x:.02f}')
+def q4aii():
+    f = lambda x:np.exp(np.cos(np.pi*x**2))
+    x_0 = 1/(np.sqrt(2))
+    h = 0.1**np.linspace(0, 21, 100)              
+    dfdx = (f(x_0+h)-f(x_0-h))/(2*h) #2nd estimation
 
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
+    error = np.abs(dfdx-(-np.sqrt(2)*np.pi))
+
+    print(f'Min e of {np.min(error)} at h = {h[np.argmin(error)]}')
+    plt.loglog(h,error)
+    plt.gca().invert_xaxis() 
+    plt.xlabel('h')
+    plt.ylabel('error')
+    plt.grid()
+    plt.show()  
+
+def q4aiii():
+    f = lambda x:np.exp(np.cos(np.pi*x**2))
+    x_0 = 1/(np.sqrt(2))
+    h = 0.1**np.linspace(0, 21, 100) 
+
+    d2fdx2 = (f(x_0+h)-2*f(x_0)+f(x_0-h))/(h**2) 
+    
+    error = np.abs(d2fdx2-(2*np.pi*(np.pi-1)))
+
+    print(f'Min e of {np.min(error)} at h = {h[np.argmin(error)]}')
+    plt.loglog(h,error)
+    plt.gca().invert_xaxis()
+    plt.xlabel('h')
+    plt.ylabel('error')
+    plt.grid()
+    plt.show()
+
+def q4bi():
+    a = 0
+    b = 10
+    subs = [10,20,40,80,160,320,640]
+    traps = []
+    for n in subs:
+        f = lambda x:x*np.exp(-np.sqrt(x))
+        x = np.linspace(a,b,n+1)
+        h = (b-a)/n
+        y = f(x)
+        trapezoidal_rule = (y[1:]+y[:-1]).sum()*h/2
+        traps += [trapezoidal_rule]
+        print(f"For a subinterval of {n}, intergral is {trapezoidal_rule}")
+    plt.plot(traps)
+    plt.plot(np.ones(len(traps))*4.669)
+    plt.show()
+
+def q4bii():
+    a = 0
+    b = 10
+    subs = [10,20,40,80,160]
+    simps = []
+    for n in subs:
+        f = lambda x:x*np.exp(-np.sqrt(x))
+        x = np.linspace(a,b,n+1)
+        h = (b-a)/n
+        y = f(x)
+        simpsons_rule = h/3*(y[0]+y[-1])+4*h/3*y[1::2].sum()+2*h/3*y[2:-1:2].sum()
+        simps += [simpsons_rule]
+        print(f"For a subinterval of {n}, intergral is {simpsons_rule}")
+    plt.plot(simps)
+    plt.plot(np.ones(len(simps))*4.669)
+    plt.show()
+
+def q4biii():
+    f = lambda x:x*np.exp(-np.sqrt(x))
+    upper_lims = [10,100,1000]
+    ints = []
+    for b in upper_lims:
+        result = quad(f,0.0,b)
+        print(f'output at upperlimit: {b} = {result[0]}')
+
+    result = quad(f,0.0,np.inf)
+    print(f"To positive infinity: {result[0]}")
 
     
 
 
 
-# Make data.
-x = np.arange(-5, 5, 0.1)
-y = np.arange(-5, 5, 0.1)
-x, y = np.meshgrid(x, y)
-z = (9-x**2)**(1/2) 
-surf(x,y,z,"")
-plt.show()
