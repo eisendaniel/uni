d62adff2cf6931504dab2ce43b01cdb9d90067b2
eisendaniel <danieleisen99@gmail.com>
2021-07-22 10:48:52 +1200

    fin

--- 2021/ECEN415/ass1/ass1_A.m
+++ 2021/ECEN415/ass1/ass1_A.m
@@ -1,15 +1,34 @@
-%%1
+%% 1
 clc;clear;
 s = tf('s');
-G1 = (20*(s^2+s+0.5)) / (s*(s+1)*(s+10))
-G2 = (20*(s^2+s+0.5)) / (s*(s-1)*(s+10))
-G3 = (s^2+3) / (s+1)^2
-G4 = (3*(s+1)) / (s*(s-10))
+figure()
+G1 = (20*(s^2+s+0.5)) / (s*(s+1)*(s+10));
+nyquist(G1)
 
-%%2
+figure()
+G2 = (20*(s^2+s+0.5)) / (s*(s-1)*(s+10));
+nyquist(G2)
+
+figure()
+G3 = (s^2+3) / (s+1)^2;
+nyquist(G3)
+
+figure()
+G4 = (3*(s+1)) / (s*(s-10));
+nyquist(G4)
+
+%% 2
+clc;clear;
+G = zpk([], -2,4);
+G.IODelay = 0.2;
+
+%% 3
 clc;clear;
 s = tf('s');
-G = zpk([], -2,4);
-GG = 4 / (s+2);
-G.IODelay = 0.2;
-GG.IODelay = 0.2;
+G = 1000 / (s+1000);
+figure();
+impulse(G)
+figure();
+step(G)
+figure();
+bode(G);
